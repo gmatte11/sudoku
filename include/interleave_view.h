3bebf32028cb0ff378e74a5d584dd399d2aac95c
Guillaume Matte <gmatte11@gmail.com>
2017-04-23 23:59:11 -0400

    Started view of rows, columns and zones with little success right now for columns view.

---
+++ include/interleave_view.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <range/v3/all.hpp>
+/* Directly taken from Eric Niebler's Calendar example.
+   https://github.com/ericniebler/range-v3/blob/master/example/calendar.cpp
+*/
+
+namespace sudoku_view
+{
+    // Flattens a range of ranges by iterating the inner
+    // ranges in round-robin fashion.
+    template<class Rngs>
+    class interleave_view : public ranges::view_facade<interleave_view<Rngs>> {
+        friend ranges::range_access;
+        std::vector<ranges::range_value_type_t<Rngs>> rngs_;
+        struct cursor;
+        cursor begin_cursor() {
+            return {0, &rngs_, ranges::view::transform(rngs_, ranges::begin)};
+        }
+    public:
+        interleave_view() = default;
+        explicit interleave_view(Rngs rngs)
+          : rngs_(std::move(rngs))
+        {}
+    };
+
+    template<class Rngs>
+    struct interleave_view<Rngs>::cursor  {
+        std::size_t n_;
+        std::vector<ranges::range_value_type_t<Rngs>> *rngs_;
+        std::vector<ranges::iterator_t<ranges::range_value_type_t<Rngs>>> its_;
+        decltype(auto) read() const {
+            return *its_[n_];
+        }
+        void next() {
+            if(0 == ((++n_) %= its_.size()))
+                ranges::for_each(its_, [](auto& it){ ++it; });
+        }
+        bool equal(ranges::default_sentinel) const {
+            return n_ == 0 && its_.end() != ranges::mismatch(its_, *rngs_,
+                std::not_equal_to<>(), ranges::ident(), ranges::end).in1();
+        }
+        CONCEPT_REQUIRES(ranges::ForwardRange<ranges::range_value_type_t<Rngs>>())
+        bool equal(cursor const& that) const {
+            return n_ == that.n_ && its_ == that.its_;
+        }
+    };
+
+    // In:  Range<Range<T>>
+    // Out: Range<T>, flattened by walking the ranges
+    //                round-robin fashion.
+    auto interleave() {
+        using namespace ranges;
+        return make_pipeable([](auto&& rngs) {
+            using Rngs = decltype(rngs);
+            return interleave_view<view::all_t<Rngs>>(
+                view::all(std::forward<Rngs>(rngs)));
+        });
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,13 @@
 #include <iostream>
 #include <string>
 
+#include "transpose_view.h"
+
 using namespace ranges;
+using namespace sudoku_view;
  
 constexpr const char *LINE_SEP = "-------------------------";
+
 
 class Grid
 {
@@ -16,31 +20,43 @@ public:
         bool fixed_:1;
         uint8_t pad_:3;
 
-        char print() const { return val_ + '0'; }
+        Cell()
+            : val_(0)
+            , fixed_(false)
+        {
+        }
+
+        char as_char() const { return val_ + '0'; }
         uint8_t val() const { return val_; }
-        operator char() const { return print(); }
+
+        operator char() const { return as_char(); }
     };
     static_assert(sizeof(Cell) == 1);
 
-    Grid()
+    any_view<char> chars() const { return data_; }
+    auto cells() { return view::all(data_); }
+
+    auto rows()
     {
-        int i = 0;
-        for (Cell & cell : data_)
-        {
-            cell.val_ = (i++ % 9) + 1;
-        }
+        return cells() | view::chunk(9);
     }
 
-    any_view<char> cells() const { return data_; }
+    auto columns()
+    {  
+        return rows() | transpose();
+    }
+
+    /*auto zones()
+    {  
+        return cells()| zone_view;
+    }*/
 
     std::array<Cell, 81> data_;
 };
 
-int main(int argc, char *argv[])
+void print(Grid const& grid)
 {
-    Grid grid;
-
-    for (auto && lines : grid.cells() | view::chunk(9) | view::chunk(3))
+    for (auto && lines : grid.chars() | view::chunk(9) | view::chunk(3))
     {
         std::cout << LINE_SEP << '\n';
         for (auto && line : lines)
@@ -51,5 +67,36 @@ int main(int argc, char *argv[])
         }
     }
     std::cout << LINE_SEP << '\n'; 
+}
+
+template <typename Rng>
+void fill_cells(Rng && rng)
+{
+    for (auto && cells : rng)
+    {
+        auto nums = view::ints(1);
+        auto i = std::begin(nums);
+        for (auto & cell : cells)
+        {
+            cell.val_ = *i++;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    Grid grid;
+
+    std::cout << "Rows\n";
+    fill_cells(grid.rows());
+    print(grid);
+
+    std::cout << "\n\nColumns\n";
+    fill_cells(grid.columns());
+    print(grid);
+
+    /*std::cout << "\n\nZones\n";
+    fill_cells(grid.zones());
+    print(grid);*/
     return 0;
 }
